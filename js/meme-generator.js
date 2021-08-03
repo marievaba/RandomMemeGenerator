d9dc4142752e322eb1b31e673fe60b66ec5ffc8b
Marie Elisabeth Vabamägi <marie@Maries-iMac.lan>
2021-08-04 01:45:14 +0300

    fix quotes

--- js/meme-generator.js
+++ js/meme-generator.js
@@ -1,6 +1,6 @@
 function generando() {
   const randomNumber = Math.floor(Math.random() * 52);
-  log("Generated random number " + randomNumber);
+  log('Generated random number ' + randomNumber);
   let imagePath = 'images/meme_' + randomNumber + '.jpg';
   let loaderElement = document.getElementById('loader');
   if (loaderElement !== null) {
@@ -11,26 +11,26 @@ function generando() {
 
 function highlightEmoticon(sliderValue) {
   if (sliderValue < 33) {
-    document.getElementById("first").style.color = "white";
-    document.getElementById("second").style.color = "black";
-    document.getElementById("third").style.color = "black";
-    log("Feelings state set to 'sad', slider value = " + sliderValue);
+    document.getElementById('first').style.color = 'white';
+    document.getElementById('second').style.color = 'black';
+    document.getElementById('third').style.color = 'black';
+    log('Feelings state set to "sad", slider value = ' + sliderValue);
   }
   if (sliderValue > 33 && sliderValue < 66) {
-    document.getElementById("first").style.color = 'black';
-    document.getElementById("second").style.color = "white";
-    document.getElementById("third").style.color = "black";
-    log("Feelings state set to 'meh', slider value = " + sliderValue);
+    document.getElementById('first').style.color = 'black';
+    document.getElementById('second').style.color = 'white';
+    document.getElementById('third').style.color = 'black';
+    log('Feelings state set to "meh", slider value = ' + sliderValue);
   }
   if (sliderValue > 66) {
-    document.getElementById("first").style.color = "black";
-    document.getElementById("second").style.color = "black";
-    document.getElementById("third").style.color = "white";
-    log("Feelings state set to 'happy', slider value = " + sliderValue);
+    document.getElementById('first').style.color = 'black';
+    document.getElementById('second').style.color = 'black';
+    document.getElementById('third').style.color = 'white';
+    log('Feelings state set to "happy", slider value = ' + sliderValue);
   }
-  document.getElementById("button").disabled = false;
+  document.getElementById('button').disabled = false;
 }
 
 function log(message) {
-  console.log("INFO LOG: " + message);
+  console.log('INFO LOG: ' + message);
 }
